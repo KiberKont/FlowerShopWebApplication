bf9572f9151eded5305c0a523634b17a2ac82b63
KiberKont <kos.kirikovv@gmail.com>
2026-08-19 15:00:25 +0300

    feat(utils): Добавить функцию экранирования HTML для защиты от XSS-атак

---
+++ FlowerShopWebApplication/wwwroot/js/utils.js
@@ -0,0 +1,10 @@
+﻿// Экранировать спецсимволы HTML для предотвращения XSS-атак
+function escapeHtml(text) {
+    if (!text) return '';
+    return String(text)
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&#039;');
+}
